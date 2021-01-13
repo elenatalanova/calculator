01234f5b9cbbc290da36eb42db06cd294deefec1
Lena Talanova <lena.talanova.ua@gmail.com>
2021-01-13 15:46:44 -0800

    Add changes to the html and js script

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
-function add(number1, number2) {
+// Business logic:
+
+const add = function(number1, number2) {
   return number1 + number2;
   }
-  
-  const number1 = parseInt(prompt("Enter a nuber:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  
+
+const substract = function(number1, number2) {
+  return number1 - number2;
+}   
+
+const multiply = function(number1, number2) {
+  return number1 * number2;
+}
+
+const divide = function(number1, number2) {
+  return number1 / number2;
+}
+
+// User Interface Logic
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+  const number1 = parseInt($("#add1").val());
+  const number2 = parseInt($("#add2").val());
   alert(add(number1, number2));
+  });
+});
+
+
