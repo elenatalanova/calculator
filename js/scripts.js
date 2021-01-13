5b9af54534d5f096501b196ab71a05b7bb468a69
Lena Talanova <lena.talanova.ua@gmail.com>
2021-01-13 13:03:14 -0800

    Add script to js and html files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+  return number1 + number2;
+  }
+  
+  const number1 = parseInt(prompt("Enter a nuber:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+  
+  alert(add(number1, number2));
