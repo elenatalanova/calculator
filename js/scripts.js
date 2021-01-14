f7c3b7409f7d804bbf63d997a72384d7918a5455
Lena Talanova <lena.talanova.ua@gmail.com>
2021-01-14 14:16:29 -0800

    add radio buttons, update user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -19,40 +19,47 @@ const divide = function(number1, number2) {
 // User Interface Logic
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function(event) {
   event.preventDefault();
-  const number1 = parseInt($("#add1").val());
-  const number2 = parseInt($("#add2").val());
+  const number1 = parseInt($("#input1").val());
+  const number2 = parseInt($("#input2").val());
+  const operator = $("input:radio[name=operator]:checked").val();
+  console.log("1st number: " + number1);
+  console.log("2nd number: " + number2);
+  console.log("operator: " + operator);
   const result = add(number1, number2);
   $("#output").text(result);
   });
 });
 
 $(document).ready(function() {
-  $("form#substract").submit(function(event) {
+  $("form#calculator").submit(function(event) {
   event.preventDefault();
   const number1 = parseInt($("#substract1").val());
   const number2 = parseInt($("#substract2").val());
+  const operator = $("input:radio[name=operator]:checked").val();
   const result = substract(number1, number2);
   $("#output2").text(result);
   });
 });
 
 $(document).ready(function() {
-  $("form#multiply").submit(function(event) {
+  $("form#calculator").submit(function(event) {
   event.preventDefault();
   const number1 = parseInt($("#multiply1").val());
   const number2 = parseInt($("#multiply2").val());
+  const operator = $("input:radio[name=operator]:checked").val();
   const result = multiply(number1, number2);
   $("#output3").text(result);
   });
 });
 
 $(document).ready(function() {
-  $("form#divide").submit(function(event) {
+  $("form#calculator").submit(function(event) {
   event.preventDefault();
   const number1 = parseInt($("#divide1").val());
   const number2 = parseInt($("#divide2").val());
+  const operator = $("input:radio[name=operator]:checked").val();
   const result = divide(number1, number2);
   $("#output4").text(result);
   });
